c836d36f5169d209a45b91b27aa068366cff4761
andoh <solomonkingsman@gmail.com>
2026-05-10 22:02:29 +0000

    add env to config

--- src/config.py
+++ src/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     database_url: str
     sentry_dsn: str | None = None
     redis_url: str
+    jwt_secret: str
+    jwt_algorithm: str
 
     model_config = SettingsConfigDict(
         env_file=".env", extra="ignore", case_sensitive=False
