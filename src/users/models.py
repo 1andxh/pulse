14fda59d047bb02adbf83edc5a58213515d6f52b
andoh <solomonkingsman@gmail.com>
2026-05-12 23:39:50 +0000

    add fullname field

--- src/users/models.py
+++ src/users/models.py
@@ -23,6 +23,7 @@ class User(Base):
     id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
+    full_name: Mapped[str | None] = mapped_column(String(255), nullable=True)
     email: Mapped[str] = mapped_column(
         String(255), unique=True, nullable=False, index=True
     )
