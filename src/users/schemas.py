ef5e5532c64d5c14566d8891c1cf68e889483451
andoh <solomonkingsman@gmail.com>
2026-05-12 23:40:11 +0000

    add oauth user schema

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -31,3 +31,12 @@ class UserResponse(BaseModel):
     created_at: datetime.datetime
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class GoogleUser(BaseModel):
+    google_sub: str
+    email: str
+    full_name: str
+
+    class Config:
+        from_attributes = True
