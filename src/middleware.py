ff3fa3a179ab03223dfb3ab7773c8c3bf8e37c38
andoh <solomonkingsman@gmail.com>
2026-05-12 15:59:04 +0000

    register cors middleware

--- src/middleware.py
+++ src/middleware.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.middleware.trustedhost import TrustedHostMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from src.config import settings
 
 
@@ -10,5 +11,13 @@ def register_middleware(app: FastAPI):
     app.add_middleware(
         TrustedHostMiddleware,
         www_redirect=True,
-        allowed_hosts=["localhost", "127.0.0.1", "127.0.0.1:6379"],
+        allowed_hosts=["localhost", "127.0.0.1", "127.0.0.1:6379", settings.api_url],
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://127.0.0.1:5173", "http://localhost:5173"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
